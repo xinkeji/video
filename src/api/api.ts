ca8531a6b191eb9c4d8fed5b1c7995f94326d433
naive <33138729+xinkeji@users.noreply.github.com>
2024-03-11 14:15:32 +0800

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,12 @@
 var jxapis = {
-  "心科技解析":"https://jx.jsonplayer.com/player/?url=",
+ 
   "全民解析":"https://jx.aidouer.net/?url=",
   'go': "https://jx.playerjy.com?url=",
   "8090g": "https://www.8090g.cn/?url=",
   "8090g2": "https://www.8090g.cn/jiexi/?url=",
   "ckmov": "https://www.ckmov.vip/api.php?url=",
   "m3u8": "https://jx.m3u8.tv/jiexi/?url=",
+  "心科技解析":"https://jx.jsonplayer.com/player/?url=",
 }
 
 export default jxapis;
