99d6c7f92a6fe1e9a1ae820429f3d97a3e517aae
naive <33138729+xinkeji@users.noreply.github.com>
2024-03-11 12:32:17 +0800

    Update Video.tsx

--- src/Video.tsx
+++ src/Video.tsx
@@ -91,23 +91,20 @@ const Video =  () => {
     </div>
     <div style={{textAlign:'center', marginTop:20}}>不定期更新解析资源，仅供学习使用，切勿用于商业。</div>
      
-        <div style={{textAlign:'center', marginTop:20}}>
-        <span style={{ fontWeight: 'bold' }}>操作指南</span>
-       
+          <div style={{textAlign:'center', marginTop:20}}>
+        <span style={{ fontWeight: 'bold' }}>操作指南</span><br />
         第&nbsp;一&nbsp;步&nbsp;进入各大视频网站，找到想要观看的VIP视频vip会员，然后复制链接（浏览器上的视频地址）<br />
         第&nbsp;二&nbsp;步&nbsp;将复制的链接粘贴到本站播放地址，并点击开始解析<br />
         第&nbsp;三&nbsp;步&nbsp;等待解析完成，即可免费观看VIP视频vip会员<br />
         想要找会员电影免费看，下方搜索电影名，就能免费观看vip会员vip付费电影电视剧<br />
-          </div>
-     <div style={{textAlign:'center', marginTop:20}}>  
-    <p>友情链接：</p> 
-   <p> <a target="_blank" title="苦心ios导航" href="http://93665.xin/">苦心ios导航</a>&nbsp;&nbsp; <a target="_blank" title="心科技圈" href="https://xkj.93665.xin/">心科技圈</a>&nbsp;&nbsp; 
- 
-   <div class="panel-body" style="text-align: center;"> 
-    <div class="container-fluid"> 
-     <p><a target="_blank" href="" class="btn btn-default btn-sm"> <span class="glyphicon glyphicon-thumbs-up"></span>微信公众号:心科技</a></p> 
-    </div> 
-     </div>
+      </div>
+      <div style={{textAlign:'center', marginTop:20}}>
+        <p>友情链接：</p>
+        <p>
+          <a target="_blank" rel="noopener noreferrer" title="苦心ios导航" href="http://93665.xin/">苦心ios导航</a>&nbsp;&nbsp;
+          <a target="_blank" rel="noopener noreferrer" title="心科技圈" href="https://xkj.93665.xin/">心科技圈</a>
+        </p>
+      </div>
 
   </div>
 }
